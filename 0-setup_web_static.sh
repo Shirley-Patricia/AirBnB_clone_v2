4abf393242d3cafb10abcf2ccb07b837ecc469b3
Shirley <shircar16@gmail.com>
2022-01-16 16:15:04 -0500

    fixing code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,11 +5,8 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-
+sudo service nginx start
 # Creating of the folders if it doesn’t already exist
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
@@ -28,10 +25,10 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group.
 # This should be recursive; everything inside should be created/owned by this user/group
 
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 
-sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}' /etc/nginx/sites-available/default
+sudo sed -i "54 i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}\n" /etc/nginx/sites-available/default
 
 sudo service nginx restart
