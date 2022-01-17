b48326a5e58332a94d6f379d6d10716bef105a74
Shirley <shircar16@gmail.com>
2022-01-17 00:02:01 -0500

    fixing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,22 +2,20 @@
 # Write a Bash script that sets up your web servers for the deployment of web_static. It must:
 # Install Nginx if it not already installed.
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -q -y install nginx
 sudo service nginx start
 # Creating of the folders if it doesn’t already exist
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/{releases/test,shared}
 
 # Create a fake HTML file 
-echo "<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Creating of a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -25,7 +23,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group.
 # This should be recursive; everything inside should be created/owned by this user/group
 
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 
