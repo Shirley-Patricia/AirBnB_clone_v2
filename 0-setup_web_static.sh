0925e00ed39d340f3993d7f6a2cb0e1766bbe9e3
Shirley <shircar16@gmail.com>
2022-01-17 12:01:15 -0500

    fixing command sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,10 +3,11 @@
 # Install Nginx if it not already installed.
 
 sudo apt-get update
-sudo apt-get -q -y install nginx
+sudo apt-get -y install nginx
 sudo service nginx start
 # Creating of the folders if it doesn’t already exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
 # Create a fake HTML file 
 sudo echo "<html>
@@ -15,7 +16,7 @@ sudo echo "<html>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 
 # Creating of a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -23,10 +24,10 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group.
 # This should be recursive; everything inside should be created/owned by this user/group
 
-sudo chown -hR ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 
-sudo sed -i "54 i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "24i location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}\n" /etc/nginx/sites-available/default
 
 sudo service nginx restart
