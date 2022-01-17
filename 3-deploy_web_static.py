73df1edec2320f54bba31f3a35fa4159c96a61a1
Shirley <shircar16@gmail.com>
2022-01-17 17:42:30 -0500

    adding comment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -34,38 +34,39 @@ def do_deploy(archive_path):
     """To distribute an archive to web servers
        Returns False if the file at the path archive_path doesn’t exist
     """
-    if not os.path.exists(archive_path):
+    if os.path.exists(archive_path):
+        """Upload the archive to the /tmp/ directory of the web server"""
+        unpack = archive_path.split("/")[-1]
+        arch_path = "/data/web_static/releases/"
+        ext = unpack.split(".")[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}{}/".format(arch_path, ext))
+
+        """Uncompress the archive to the folder
+        /data/web_static/releases/<archive filename without extension>
+        on the web server"""
+        run("tar -xzf /tmp/{} -C {}{}/".format(unpack, arch_path, ext))
+
+        """Delete the archive from the web server"""
+        run("rm /tmp/{}".format(unpack))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(arch_path, ext))
+        run("rm -rf {}{}/web_static".format(arch_path, ext))
+
+        """Delete the symbolic link /data/web_static/current
+            from the web server"""
+        run("rm -rf /data/web_static/current")
+        """Create a new the symbolic link /data/web_static/current on the
+            web server, linked to the new version of your code
+            (/data/web_static/releases/<archive filename without extension>)
+        """
+        run("ln -s {}{}/ /data/web_static/current".format(arch_path, ext))
+        return True
+    else:
         return False
-
-    """Upload the archive to the /tmp/ directory of the web server"""
-    unpack = archive_path.split("/")[-1]
-    arch_path = "/data/web_static/releases/"
-    ext = unpack.split(".")[0]
-    put(archive_path, "/tmp/")
-    run("mkdir -p {}{}/".format(arch_path, ext))
-
-    """Uncompress the archive to the folder
-    /data/web_static/releases/<archive filename without extension>
-    on the web server"""
-    run("tar -xzf /tmp/{} -C {}{}/".format(unpack, arch_path, ext))
-
-    """Delete the archive from the web server"""
-    run("rm /tmp/{}".format(unpack))
-    run("mv {0}{1}/web_static/* {0}{1}/".format(arch_path, ext))
-    run("rm -rf {}{}/web_static".format(arch_path, ext))
-
-    """Delete the symbolic link /data/web_static/current
-        from the web server"""
-    run("rm -rf /data/web_static/current")
-    """Create a new the symbolic link /data/web_static/current on the
-        web server, linked to the new version of your code
-        (/data/web_static/releases/<archive filename without extension>)
-    """
-    run("ln -s {}{}/ /data/web_static/current".format(arch_path, ext))
-    return True
 
 
 def deploy():
+    """creates and distributes an archive"""
     archive_path = do_pack()
     if archive_path:
         archive_created = do_deploy(archive_path)
