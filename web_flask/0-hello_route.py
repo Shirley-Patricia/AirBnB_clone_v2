f27ae17accf9a5a9d20265ce1ccb29f9ed426629
Shirley <shircar16@gmail.com>
2022-01-29 18:23:16 -0500

    changing strict_slashes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,8 +4,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def hello():
-	return 'Hello HBNB!'
+    return 'Hello HBNB!'
