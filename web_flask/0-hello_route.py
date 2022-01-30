f1ce419d4b4ba2d4667a8ef0d948161e2f48486c
Shirley <shircar16@gmail.com>
2022-01-29 19:02:47 -0500

    added flask_script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 
+from flask_script import Manager
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,3 +10,6 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    manager.run()
